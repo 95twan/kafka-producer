9dd65cd7f741e297bec2b1c0ebff2ed7d420bc84
taewan <95twan@gmail.com>
2022-09-16 03:46:08 +0900

    kafka producer init

---
+++ src/main/java/com/backseju/kafkaproducer/KafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.backseju.kafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backseju/kafkaproducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backseju.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
